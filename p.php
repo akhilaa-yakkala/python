a1d785fa0589d64dd9030fd2096974540301ed45
akhila <akhila.yakkala@quadrantresource.com>
2023-10-03 11:32:42 +0530

    FIRST

---
+++ p.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+<html>
+    <head>
+        <title>
+            Amazon
+</title>
+<link rel="stylesheet" href="style.css"type="text/css">
+</head>
+<body>
+  <div class="container1">
+    <p>Amazon</p>
+  </div>
+</body>
+</html>
